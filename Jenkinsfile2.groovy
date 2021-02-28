509b5d5e0c6f19b9c6f50728545c2f7965101ad4
rakeshdyx <rakeshdyx@gmail.com>
2021-02-28 07:34:26 +0530

    Jenkinsfile 2 added

---
+++ Jenkinsfile2.groovy
@@ -0,0 +1,10 @@
+pipeline{
+    agent any
+    stages{
+        stage('Example...'){
+            step{
+                echo "Running ${env.BUILD_ID} on Jenkin URL-${env.JENKINS_URL}"
+            }
+        }
+    }
+}
