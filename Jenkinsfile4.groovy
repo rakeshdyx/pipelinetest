9376e946c6d2df7410d22a85a378e94b7541110d
rakeshdyx <rakeshdyx@gmail.com>
2021-02-28 16:45:05 +0530

    jenkins4 added

--- Jenkinsfile4.groovy
+++ Jenkinsfile4.groovy
@@ -4,15 +4,16 @@ pipeline {
         CC = """${sh(
             returnStdout: true,
             script: 'echo "clang"'
-        ) }"""
+        )}"""
+        EXIT_STATUS = """${sh(
+                returnStatus: true,
+                script = 'exit 1'
+        )}"""
     }
     stages {
         stage('Example..') {
             environment {
-            DEBUG_FLAGS = """${sh(
-                returnStatus: true,
-                script = 'exit 1'
-            )}"""
+                DEBUG_FLAGS = '-g'
             }
             steps {
                 sh "printenv"
